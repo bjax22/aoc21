2197515e2f25b03b088268951932dd692327dbfe
Brandon Jackson <brandon.jackson@bestbuy.com>
2023-11-20 15:14:07 -0600

    2015-day2-inprogress

--- 2015/02/a.rb
+++ 2015/02/a.rb
@@ -1,9 +1,21 @@
-inputfilename = "./test-input.txt"        #test input file
-#inputfilename = "./prod-input.txt"        #prod input file
-debug = 1 > 2
+#inputfilename = "./test-input.txt"        #test input file
+inputfilename = "./prod-input.txt"        #prod input file
+debug = 1 < 2
 debug_input = 1 > 2
 input = File.readlines(inputfilename)
 
 if debug_input 
     puts input
 end
+
+l,w,h = 0
+total_paper_area = 0
+input.each do |line|
+    l,w,h = line.split('x').map {|s| s.to_i}
+    puts "l: #{l}; w: #{w}; h: #{h}" if debug
+    area = [l*h,w*h,l*w].sum * 2 + [l*h,w*h,l*w].min
+    puts "Package area: #{area}"
+    total_paper_area += area
+end
+
+puts "Total paper needed: #{total_paper_area}"
--- 2015/02/b.rb
+++ 2015/02/b.rb
@@ -1,9 +1,24 @@
-inputfilename = "./test-input.txt"        #test input file
-#inputfilename = "./prod-input.txt"        #prod input file
-debug = 1 > 2
+#inputfilename = "./test-input.txt"        #test input file
+inputfilename = "./prod-input.txt"        #prod input file
+debug = 1 < 2
 debug_input = 1 > 2
 input = File.readlines(inputfilename)
 
 if debug_input 
     puts input
 end
+
+l,w,h = 0
+total_paper_area = 0
+input.each do |line|
+    l,w,h = line.split('x').map {|s| s.to_i}
+    puts "l: #{l}; w: #{w}; h: #{h}" if debug
+    s1 = [l,w,h].sort[0]
+    s2 = [l,w,h].sort[1]
+    
+#    area = [l*h,w*h,l*w].sum * 2 + [l*h,w*h,l*w].min
+#    puts "Package area: #{area}" if debug
+    total_paper_area += area
+end
+
+puts "Total paper needed: #{total_paper_area}"
