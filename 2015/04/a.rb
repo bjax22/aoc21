cea3c6e82693ea3d803f8db508f38f769903235e
Brandon Jackson <brandon.jackson@bestbuy.com>
2023-11-20 19:30:27 -0600

    2015 Day 4 pt 1 complete

---
+++ 2015/04/a.rb
@@ -0,0 +1,19 @@
+require 'digest'
+
+#inputfilename = "./test-input.txt"        #test input file
+inputfilename = "./prod-input.txt"        #prod input file
+debug = 1 < 2
+debug_input = 1 > 2
+input = File.read(inputfilename)
+
+puts input if debug_input 
+
+counter = 0
+loop do 
+    counter += 1
+    hash_this = input + counter.to_s
+    output = Digest::MD5.hexdigest hash_this
+    puts "counter: #{counter}; hash_this: #{hash_this}; hash: #{output}" if debug
+    break if output[0..4] == "00000"
+end
+
---
+++ 2015/04/b.rb
@@ -0,0 +1,7 @@
+inputfilename = "./test-input.txt"        #test input file
+#inputfilename = "./prod-input.txt"        #prod input file
+debug = 1 > 2
+debug_input = 1 > 2
+input = File.read(inputfilename)
+
+puts input if debug_input 
