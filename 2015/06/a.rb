df280ea1f21495c52d07caf69197b810ba460a37
Brandon Jackson <brandon.jackson@bestbuy.com>
2023-11-24 08:24:04 -0600

    Day 6 part 1 complete

--- 2015/06/a.rb
+++ 2015/06/a.rb
@@ -1,12 +1,14 @@
-inputfilename = "./test-input.txt"        #test input file
-#inputfilename = "./prod-input.txt"        #prod input file
+# https://adventofcode.com/2015/day/6
+
+#inputfilename = "./test-input.txt"        #test input file
+inputfilename = "./prod-input.txt"        #prod input file
 debug = 1 > 2
 debug_input = 1 > 2
 input = File.readlines(inputfilename)
 
 puts input if debug_input 
 
-# grid = Array.new(1000) { Array.new(1000,0) }
+grid = Array.new(1000) { Array.new(1000,0) }
 
 def parse_line(line)
     words = line.split
@@ -26,6 +28,26 @@ end
 # MAIN
 #########
 input.each do |line|
-    inst = parse_line(line)
-    p inst
+    instructions = parse_line(line)
+    p instructions if debug
+    startX,startY = instructions[1].split(',')
+    endX,endY = instructions[2].split(',')
+    (startX.to_i ..endX.to_i).each do |x| 
+        (startY.to_i .. endY.to_i).each do |y|
+            case instructions[0]
+                when "on"
+                    grid[x][y] = 1
+                when "off"
+                    grid[x][y] = 0
+                when "toggle"
+                    grid[x][y] = (grid[x][y] - 1).abs()
+                else
+            end
+        end
+    end
 end
+
+sumOfAllLights = 0
+(0..999).each {|x| sumOfAllLights += grid[x].sum}
+
+puts "There are #{sumOfAllLights} lit."
