462f6e630bc5405effa79197b0bba354a1dd83b7
Brandon Jackson <brandon.jackson@bestbuy.com>
2023-11-24 08:34:57 -0600

    Day 6 part 2 complete

--- 2015/06/b.rb
+++ 2015/06/b.rb
@@ -1,7 +1,54 @@
-inputfilename = "./test-input.txt"        #test input file
-#inputfilename = "./prod-input.txt"        #prod input file
+# https://adventofcode.com/2015/day/6
+
+#inputfilename = "./test-input.txt"        #test input file
+inputfilename = "./prod-input.txt"        #prod input file
 debug = 1 > 2
 debug_input = 1 > 2
 input = File.readlines(inputfilename)
 
 puts input if debug_input 
+
+grid = Array.new(1000) { Array.new(1000,0) }
+
+def parse_line(line)
+    words = line.split
+    if words[0] == "toggle"
+        command = words[0]
+        fBulb = words[1]
+        lBulb = words[-1]
+    else
+        command = words[1]
+        fBulb = words[2]
+        lBulb = words[-1]
+    end
+    returnArr = [command,fBulb,lBulb]
+end
+
+#########
+# MAIN
+#########
+input.each do |line|
+    instructions = parse_line(line)
+    p instructions if debug
+    startX,startY = instructions[1].split(',')
+    endX,endY = instructions[2].split(',')
+    (startX.to_i ..endX.to_i).each do |x| 
+        (startY.to_i .. endY.to_i).each do |y|
+            case instructions[0]
+                when "on"
+                    grid[x][y] += 1
+                when "off"
+                    grid[x][y] -= 1
+                    grid[x][y] = 0 if grid[x][y] < 0
+                when "toggle"
+                    grid[x][y] += 2
+                else
+            end
+        end
+    end
+end
+
+sumOfAllLights = 0
+(0..999).each {|x| sumOfAllLights += grid[x].sum}
+
+puts "Total brightness is: #{sumOfAllLights}."
